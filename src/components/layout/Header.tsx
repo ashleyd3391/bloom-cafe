f4a534c0dd4f6743007aff9e1ea21c2946a00ed4
ashleyd3391 <ashleyd3391@gmail.com>
2026-07-01 14:41:12 -0500

    add working nav panel with toggle state

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,14 +1,32 @@
+import { useState } from 'react';
 import styles from './Header.module.css';
 import bloomLogo from '../../assets/bloom-logo.png';
 
 function Header() {
+    const [nav, setNav] = useState(false);
     return (
         <header className={styles.header}>
             <h1 className={styles.companyName}>bloom</h1>
             <img className={styles.logo} src={bloomLogo} alt="Bloom Logo" />
-            <button className={styles.navButton}>•••</button>
+            <button className={styles.navButton} onClick={() => setNav(!nav)}>
+                •••
+            </button>
+                
+        {nav && (
+            <div  className={styles.navMenu}>
+                <button className={styles.closeButton} onClick={() => setNav(false)}>X</button>
+
+                <ul className={styles.navList}>
+                    <li className={styles.navItem}>Menu</li>
+                    <li className={styles.navItem}>Cart</li>
+                    <li className={styles.navItem}>Contact</li>
+                </ul>
+            </div>
+        )}
+
         </header>
     )
+
 }
 
 export default Header
