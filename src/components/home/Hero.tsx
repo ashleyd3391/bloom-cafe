960bf84c1a5ee9f0ba44659039197b9d92d121fc
ashleyd3391 <ashleyd3391@gmail.com>
2026-07-03 11:29:30 -0500

    Finished hero styling

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -1,12 +1,11 @@
 import styles from './Hero.module.css';
-import flowerBackground from '../../assets/flower-background.png';
 import matcha from '../../assets/matcha.png';
 function Hero() {
     return (
         <section className={styles.hero}>
             <h1 className={styles.title}>Welcome!</h1>
-            <p className={styles.description}>After one sip of our matcha, we promise you’ll bloom into a new you!</p>
-            <img src={matcha} alt="A glass of iced matcha latte on a wooden coaster" />
+            <p className={styles.description}>After one sip of our matcha, <br></br> we promise you’ll bloom into a <br></br> new you!</p>
+            <img className={styles.image} src={matcha} alt="A glass of iced matcha latte on a wooden coaster" />
         </section>
     )
 }
