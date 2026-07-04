23e9c346445825227893a357ef37e84181a33ad3
ashleyd3391 <ashleyd3391@gmail.com>
2026-07-04 13:01:04 -0500

    Completed mission styling

--- src/components/home/Mission.tsx
+++ src/components/home/Mission.tsx
@@ -1,13 +1,15 @@
 import styles from './Mission.module.css';
 import matcha2 from '../../assets/matcha2.png';
 import matcha3 from '../../assets/matcha3.png';
+import bloomLogo from '../../assets/bloom-logo.png';
 
 function Mission() {
     return (
         <section className={styles.mission}>
+            <img src={bloomLogo} alt="Bloom Logo" className={styles.logo} />
             <p className={styles.description}>Here at bloom, matcha is more than a drink. It’s an ritual of calm and time to reflect. </p>
-            <img src={matcha2} alt="Matcha powder freshly whisked" className={styles.image} />
-            <p className={styles.description}>Our unique blend improves your mood and attention to focus on being a better you today!</p>
+            <img src={matcha2} alt="Matcha powder freshly whisked"  />
+            <p className={styles.description2}>Our unique blend improves your <span style={{ color: '#C94042' }}>mood</span> and <span style={{ color: '#C94042' }}>attention</span> to focus on being a better you <br></br> today!</p>
             <img src={matcha3} alt="Iced matcha latte" className={styles.image} />
         </section>
     )
